5253b1de2b944fbf849637f1b839c452297df34c
jothishiva123 <113039401+jothishiva123@users.noreply.github.com>
2022-09-28 17:47:37 +0530

    Create Jenkinsfile.groovy

---
+++ vars/Jenkinsfile.groovy
@@ -0,0 +1,35 @@
+def call (def mvncmd) {
+	try {
+		echo "*****************************************************"
+		echo "Maven build Started..."
+		bat 'mvn '+mvncmd
+		echo "Maven build completed successfully"
+		echo "*****************************************************"
+	}
+	catch(Exception th) {
+		echo "Maven build Failed. Please Check console log"
+		throw th;
+	}
+}
+def call (def giturl, def branch,def Git_Cred_ID) {
+	echo ""
+	echo "********** Git URL:  ${giturl} **********"
+	echo ""
+	echo "********** Git Branch:  ${branch} **********"
+	echo ""
+	if(giturl.contains("github.com"))
+	{
+	checkout([$class: 'GitSCM', branches: [[name: branch ]], extensions: [], userRemoteConfigs: [
+			[credentialsId: Git_Cred_ID, url: giturl]
+		]])
+	}
+	else
+	{
+		error("Please Provide valid github url")
+	}
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
